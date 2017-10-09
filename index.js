411863492b2be7fdd5f83e07cc65bdec0d43e514
ambar13 <ambar13_9@hotmail.com>
2017-10-09 12:49:59 -0500

    subiendo todo

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ function isValidCard(numValida){
     var LongitudCadena = numValida.Length;
     var reversCard = numValida.reverse;
   for (var i=0; i>numValida; i++){
+        var array = [];
+        var sum = reversCard[i];
             if (i % 2 == 0){
-              intImpar += (parseInt(arrNumero.values(i)) – 48);
+              return 
     }else{
        var intTemp = (Convert.ToInt32(arrNumero[i]) – 48) * 2;
        if (intTemp > 9){
@@ -20,21 +22,3 @@ function isValidCard(numValida){
   }
   }
 }
-
-
-// 1 public static  bool ValidaModulo10(string strNumeroValidar){     {
-//       strNumeroValidar = Regex.Replace(strNumeroValidar, @”[^0-9]”, “”);
-
-//   4         int LongitudCadena = strNumeroValidar.Length;
-
-//   5         int intImpar = 0;
-
-//   6         int intUniforme = 0;
-
-//   7
-
-//   8         char[] arrNumero = new char[LongitudCadena];
-
-//   9         arrNumero = strNumeroValidar.ToCharArray();
-
-//  10         Array.Reverse(arrNumero, 0, LongitudCadena);
