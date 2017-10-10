0a50457b956eadb3d6a3f017acc4c52855194ad0
ambar13 <32307145+ambar13@users.noreply.github.com>
2017-10-10 12:54:29 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+var numValida = [1,2,3,4,5,6,7,8,9,1,2,3,4,5,6];
+function isValidCard(numValida){
+    var LongitudCadena = numValida.Length;
+    var reversCard = numValida.reverse;
+  for (var i=0; i>numValida; i++){
+        var array = [];
+        var sum = reversCard[i];
+            if (i % 2 == 0){
+              var mult = mult * 2;
+              var sumD = mult % 10;
+            
+    }else{
+       var intTemp = (Convert.ToInt32(arrNumero[i]) – 48) * 2;
+       if (intTemp > 9){
+           intTemp = intTemp – 9;
+                  }
+  intUniforme += intTemp;
+   }
+  }
+  if ((intImpar + intUniforme) % 10 == 0)
+  return true;
+  else
+  return false;
+  }
+  prompt("Ingresa numero de tarjeta" + numValida+ "es:" +intUniforme);
+
